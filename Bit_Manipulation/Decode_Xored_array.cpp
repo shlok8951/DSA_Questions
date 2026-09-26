fd175cdd818ba7da6071abbe6e89463f9fbb57bd
Shlok Jain <shlokj771@gmail.com>
2026-09-26 23:08:36 +0530

    Implement using the property of XOR
    Added a decoding function to reconstruct the original array from the encoded array using XOR operations.

--- Bit_Manipulation/Decode_Xored_array.cpp
+++ Bit_Manipulation/Decode_Xored_array.cpp
@@ -1,5 +1,7 @@
 /*
 Problem-> Given a array and the starting element of resultant arrat find the other element in which previous and urrunt value Xor is equal to value of array element.
+Property : a^b = c
+           a^c = b
   */
 
 class Solution {
@@ -8,11 +10,16 @@ public:
         int n = encoded.size();
         vector<int> result;
         result.push_back(first);
+     //------------------------- Solution 1-----------------
         for(int i =0;i<n;i++){
             int j =0;
             while((result[i]^j)!=encoded[i])j++;
             result.push_back(j);
         }
+     //-------------------Solution 2---------------
+       for(int i =0;i<n;i++){
+        result.push_back(result[i]^encoded[i]);
+       }
         return result;
         
     }
